3bfe5968b355f3607ffe2ff1409644bd0aaa574e
Pedro Henrique Castilhos Fraga <pedro.castilhos.fraga@gmail.com>
2025-08-12 16:46:18 -0300

    Iniciando a calculadora

---
+++ script.js
@@ -0,0 +1,98 @@
+function clear() {
+    let res = getElementById("res");
+    res.innerHTML = " ";
+}
+
+function parenteses() {
+    let res = getElementById("res");
+    res.innerHTML = "(";
+}
+
+function porcento() {
+    let res = getElementById("res");
+    res.innerHTML = res.value / 100;
+}
+
+function soma() {
+    let res = getElementById("res");
+    res.innerHTML = "+";
+}
+
+function one() {
+    let res = getElementById("res");
+    res.innerHTML = "1";
+}
+
+function two() {
+    let res = getElementById("res");
+    res.innerHTML = "2";
+}
+
+function three() {
+    let res = getElementById("res");
+    res.innerHTML = "3";
+}
+
+function subtracao() {
+    let res = getElementById("res");
+    res.innerHTML = "-";
+}
+
+function four() {
+    let res = getElementById("res");
+    res.innerHTML = "4";
+}
+
+function five() {
+    let res = getElementById("res");
+    res.innerHTML = "5";
+}
+
+function six() {
+    let res = getElementById("res");
+    res.innerHTML = "6";
+}
+
+function multiplicacao() {
+    let res = getElementById("res");
+    res.innerHTML = "*";
+}
+
+function seven() {
+    let res = getElementById("res");
+    res.innerHTML = "7";
+}
+
+function eight() {
+    let res = getElementById("res");
+    res.innerHTML = "8";
+}
+
+function nine() {
+    let res = getElementById("res");
+    res.innerHTML = "9";
+}
+
+function divisao() {
+    let res = getElementById("res");
+    res.innerHTML = "/";
+}
+
+function remove() {
+    let res = getElementById("res");
+}
+
+function zero() {
+    let res = getElementById("res");
+    res.innerHTML = "0";
+}
+
+function ponto() {
+    let res = getElementById("res");
+    res.innerHTML = ".";
+}
+
+function equals() {
+    let res = getElementById("res");
+    res.innerHTML = "=";
+}
